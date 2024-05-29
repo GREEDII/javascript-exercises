c36d3fa47cf905a17ea90b9c88bdda19ff81de3d
GREEDII <103893887+GREEDII@users.noreply.github.com>
2024-05-29 15:00:11 +0300

    finished exercies 1-7.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  for (let i = 0; i < arr.length; i++) {
+    // compare each args element to arr[i]
+    for (let j = 0; j < args.length; j++) {
+      if (arr[i] === args[j]) {
+        arr.splice(i, 1);
+        // reset i because splice messes with indexes
+        i = 0;
+      }
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  temp = (temp - 32) * (5 / 9);
+  temp = temp.toFixed(1);
+  return parseFloat(temp);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  temp = temp * (9 / 5) + 32;
+  temp = temp.toFixed(1);
+  return parseFloat(temp);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
